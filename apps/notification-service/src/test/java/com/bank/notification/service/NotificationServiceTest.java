4e9cb3a1cb0e73c830816127a837e625f9786d24
qieqiuyue <56438921+qieqiuyue@users.noreply.github.com>
2026-06-09 16:23:49 +0800

    [FIX] NotificationServiceTest: add missing eq import

--- apps/notification-service/src/test/java/com/bank/notification/service/NotificationServiceTest.java
+++ apps/notification-service/src/test/java/com/bank/notification/service/NotificationServiceTest.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
